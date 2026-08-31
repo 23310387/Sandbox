09a2137f32494d291baab68318e4c8da0a414f39
Estefhany <a23310387@live.ceti.mx>
2026-08-30 19:04:58 -0600

    pn

---
+++ controllers/controlador_pendulo/build/release/controlador_pendulo.d
@@ -0,0 +1,6 @@
+build/release/controlador_pendulo.o: controlador_pendulo.cpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp
